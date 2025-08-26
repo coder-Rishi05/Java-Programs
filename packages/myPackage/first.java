b90fc60b5407fa4ddcf3eb98ee3d1b9d2657aa13
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-26 23:33:37 +0530

    added pkgs using qualified way

--- packages/myPackage/first.java
+++ packages/myPackage/first.java
@@ -1,8 +1,13 @@
-java.utils.Date();
+// unqualified way od using : java package
+
+import java.util.Date;
 
 class A {
     public static void main(String st[]){
      Date d = new Date();
-     System.out.println(d.getDate());   
+     System.out.println( "Today's date is : " + d.getDate());   
+     System.out.println( "Current Month is : "+  d.getMonth() + 1);   
+     System.out.print( "Current year is : " );   
+     System.out.print( d.getYear() + 1900 );   
     }
 }
