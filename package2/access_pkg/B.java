29710162b981599152a15f38b2d5e756435e79c5
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-28 22:38:49 +0530

    added pkg p1

---
+++ package2/access_pkg/B.java
@@ -0,0 +1,13 @@
+package p1;
+
+class A {
+    
+}
+
+public class B {
+
+    public static void main(String st[]){
+
+    System.out.println("This is public class inside the package.");
+    }
+}
---
+++ package2/custom_pkg/first.java
@@ -0,0 +1,11 @@
+package p1;
+
+class A {
+    public static void main(String st[]){
+        System.out.println("default packages in java");
+    }
+}
+
+class B {
+    
+}
